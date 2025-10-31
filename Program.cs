3de350fc3bf9b4be7dc08087dec50b48fbbfc44f
Manish <manish.mishra.261001@gmail.com>
2025-10-31 20:48:49 +0530

    Program.cs modified 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of rows for the diamond (half): ");
+        if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+        {
+            Console.WriteLine("Please enter a positive integer.");
+            return;
+        }
+
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(new string(' ', n - i));
+            Console.WriteLine(new string('*', 2 * i - 1));
+        }
+
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--)
+        {
+            Console.Write(new string(' ', n - i));
+            Console.WriteLine(new string('*', 2 * i - 1));
+        }
+    }
+}
